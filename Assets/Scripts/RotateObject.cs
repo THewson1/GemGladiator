3d6d2f1d8262080598e1cf036076f52f4b0f293a
AIE\s166149 <Johnfaz1994@hotmail.com>
2017-10-23 10:01:27 +1030

    Initial commit of Unity scene

---
+++ Assets/Scripts/RotateObject.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateObject : MonoBehaviour {
+
+	public float XRot;
+	public float YRot;
+	public float ZRot;
+		
+	// Update is called once per frame
+	void Update () 
+	{
+		//rotates object
+		transform.Rotate (XRot * Time.deltaTime, YRot * Time.deltaTime, ZRot * Time.deltaTime, Space.Self);		
+	}
+}
